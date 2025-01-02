fd568403268a1807326dfdce33a6cad2db94b6fd
xz <xuezhen00@163.com>
2025-01-02 17:06:28 +0800

    a

--- pages/webview/webview.vue
+++ pages/webview/webview.vue
@@ -1,18 +1,46 @@
 <template>
-	<web-view :src="url"></web-view>
+	<view>
+		<web-view :src="url" :webview-styles="webviewStyles"></web-view>
+	</view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				url: ''
+				url: '',
+				webviewStyles: {
+					progress: {
+						color: '#FF2442'
+					}
+				}
 			}
 		},
 		onLoad(options) {
 			if (options.url) {
-				this.url = decodeURIComponent(options.url)
+				this.url = decodeURIComponent(options.url);
+				// 设置定时器，每3秒刷新一次页面
+				this.refreshTimer = setInterval(() => {
+					this.refreshWebview();
+				}, 3000);
+			}
+		},
+		onUnload() {
+			// 清除定时器
+			if (this.refreshTimer) {
+				clearInterval(this.refreshTimer);
+			}
+		},
+		methods: {
+			refreshWebview() {
+				// 添加时间戳参数来强制刷新缓存
+				const timestamp = new Date().getTime();
+				const separator = this.url.includes('?') ? '&' : '?';
+				this.url = `${this.url}${separator}t=${timestamp}`;
 			}
 		}
 	}
-</script> 
+</script>
+
+<style>
+</style> 
--- unpackage/dist/dev/mp-weixin/pages/webview/webview.js
+++ unpackage/dist/dev/mp-weixin/pages/webview/webview.js
@@ -3,18 +3,39 @@ const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      url: ""
+      url: "",
+      webviewStyles: {
+        progress: {
+          color: "#FF2442"
+        }
+      }
     };
   },
   onLoad(options) {
     if (options.url) {
       this.url = decodeURIComponent(options.url);
+      this.refreshTimer = setInterval(() => {
+        this.refreshWebview();
+      }, 3e3);
+    }
+  },
+  onUnload() {
+    if (this.refreshTimer) {
+      clearInterval(this.refreshTimer);
+    }
+  },
+  methods: {
+    refreshWebview() {
+      const timestamp = (/* @__PURE__ */ new Date()).getTime();
+      const separator = this.url.includes("?") ? "&" : "?";
+      this.url = `${this.url}${separator}t=${timestamp}`;
     }
   }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: $data.url
+    a: $data.url,
+    b: $data.webviewStyles
   };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
