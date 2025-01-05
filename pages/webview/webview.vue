a740f1e5763ddb4852b0bcda5eb73428bfba98c4
xz <xuezhen00@163.com>
2025-01-05 20:32:58 +0800

    a

--- pages/webview/webview.vue
+++ pages/webview/webview.vue
@@ -1,18 +1,85 @@
 <template>
-	<web-view :src="url"></web-view>
+	<view class="container">
+		<view class="header" :style="{ paddingTop: statusBarHeight + 'px' }">
+			<view class="nav-bar">
+				<view class="back-btn" @click="goBack">
+					<text class="back-icon">〈</text>
+				</view>
+			</view>
+		</view>
+		<web-view :src="url"></web-view>
+	</view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				url: ''
+				url: '',
+				statusBarHeight: 0
 			}
 		},
 		onLoad(options) {
 			if (options.url) {
 				this.url = decodeURIComponent(options.url)
 			}
+			const sysInfo = uni.getSystemInfoSync();
+			this.statusBarHeight = sysInfo.statusBarHeight;
+		},
+		methods: {
+			goBack() {
+				uni.navigateBack();
+			}
 		}
 	}
-</script> 
+</script>
+
+<style>
+page {
+	background-color: #000000;
+}
+
+.container {
+	min-height: 100vh;
+	background-color: #000000;
+}
+
+.header {
+	background: #000000;
+	position: fixed;
+	width: 100%;
+	z-index: 100;
+}
+
+.nav-bar {
+	height: 88rpx;
+	display: flex;
+	align-items: center;
+	position: relative;
+}
+
+.back-btn {
+	position: absolute;
+	left: 0;
+	height: 88rpx;
+	width: 88rpx;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.back-icon {
+	font-size: 38rpx;
+	color: #ffffff;
+	font-weight: normal;
+	transform: scale(1.3, 1);
+}
+
+uni-page-body {
+	background-color: #000000;
+}
+
+uni-page-wrapper {
+	background-color: #000000;
+}
+</style> 
--- unpackage/dist/dev/mp-weixin/pages/webview/webview.js
+++ unpackage/dist/dev/mp-weixin/pages/webview/webview.js
@@ -3,18 +3,28 @@ const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      url: ""
+      url: "",
+      statusBarHeight: 0
     };
   },
   onLoad(options) {
     if (options.url) {
       this.url = decodeURIComponent(options.url);
     }
+    const sysInfo = common_vendor.index.getSystemInfoSync();
+    this.statusBarHeight = sysInfo.statusBarHeight;
+  },
+  methods: {
+    goBack() {
+      common_vendor.index.navigateBack();
+    }
   }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: $data.url
+    a: common_vendor.o((...args) => $options.goBack && $options.goBack(...args)),
+    b: $data.statusBarHeight + "px",
+    c: $data.url
   };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
