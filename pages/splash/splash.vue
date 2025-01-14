5c1bfeb58f9d7045e184e4685eec0a3fa35c7da9
xz <xuezhen00@163.com>
2025-01-15 01:54:54 +0800

    a

--- pages/splash/splash.vue
+++ pages/splash/splash.vue
@@ -77,7 +77,7 @@ export default {
 	font-weight: bold;
 	text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.8), 4px 4px 0 rgba(0, 0, 0, 0.6), 6px 6px 0 rgba(0, 0, 0, 0.4);
 	display: flex;
-	animation: fadeInOut 6s ease-in-out infinite;
+	animation: fadeInOut 9s ease-in-out infinite;
 }
 
 .char {
@@ -93,6 +93,6 @@ export default {
 	font-size: 24px;
 	background: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0));
 	padding: 10px 0;
-	animation: fadeInOut 6s ease-in-out infinite;
+	animation: fadeInOut 9s ease-in-out infinite;
 }
 </style> 
